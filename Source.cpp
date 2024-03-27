a79fbe717f5eef8fc11b24cd6778e184721b2076
amosreginaldjr <98925229+amosreginaldjr@users.noreply.github.com>
2024-03-27 13:25:59 -0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,48 @@
 #include<Windows.h>
 
-int main() {
+class CursorCenter
+{
+private:
     // Get the screen dimensions
     unsigned short int screenWidth = GetSystemMetrics(SM_CXSCREEN);
     unsigned short int screenHeight = GetSystemMetrics(SM_CYSCREEN);
-
     // Calculate the center coordinates
     unsigned short int centerX = screenWidth / 2;
     unsigned short int centerY = screenHeight / 2;
+public:
+    void moveCursor();
+    unsigned short int setCursorXLocation(unsigned short int cursorXDesiredLocation);
+    unsigned short int setCursorYLocation(unsigned short int cursorYDesiredLocation);
+};
 
-    // Move the mouse cursor to the center
+void CursorCenter::moveCursor()
+{
     while (!(GetKeyState('0') & 0x8000))
     {
         SetCursorPos(centerX, centerY);
         Sleep(500);
     }
+}
+
+unsigned short int CursorCenter::setCursorXLocation(unsigned short int cursorXDesiredLocation)
+{
+    if (cursorXDesiredLocation > screenWidth)
+        return centerX;
+    this->centerX = cursorXDesiredLocation;
+}
+unsigned short int CursorCenter::setCursorYLocation(unsigned short int cursorYDesiredLocation)
+{
+    if (cursorYDesiredLocation > screenWidth)
+        return centerY;
+    this->centerY = cursorYDesiredLocation;
+}
+
+int main() 
+{
+    CursorCenter cc;
+    //cc.setCursorXLocation(7000);
+    //cc.setCursorYLocation(30);
+    cc.moveCursor();
+
     return 0;
 }
